3d9f0f8ebdac81ab1414b60c11ad8cb3a1774ecc
Shailesh Kumar <shailesh.sundram@gmail.com>
2018-08-18 18:37:46 +0530

    Install express and setup a very basic express config

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-const message = 'Hello World!';
-console.log(message);
+import express from 'express';
+
+const PORT = 3000;
+const server = express();
+
+server.listen(3000, () => console.log(`Server started on port ${PORT}`));
